f3e0a2705b9dbe6b4ad90750347a3e7dc246a317
Thomas Catz <thomas@spleatapp.com>
2022-01-23 15:55:47 -0300

    rails new

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -3,4 +3,19 @@ class FlatsController < ApplicationController
   def index
     @flats = Flat.all
   end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flats_params)
+    @flat.save
+  end
+
+  private
+
+  def flats_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
